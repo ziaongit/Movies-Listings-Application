3ce6d6f373b7662ed5bc204faae6f462922ab334
Zia <zia_gt@yahoo.com>
2017-07-14 04:27:57 +0200

    Movie detail funtionality added

--- resources/javascripts/main.js
+++ resources/javascripts/main.js
@@ -27,11 +27,10 @@ function getMovies(searchText) {
             output +=   '<div class="well text-center">';
             output +=           '<img onerror="handleMissingImg(this);" src="http://image.tmdb.org/t/p/w185/'+movie.poster_path+'">';
             output +=       '<h5>'+ movie.title+'</h5>';
-            output +=       '<a onclick=movieSelected("'+movie.id+' class="btn btn-primary" href="#")>Movie Details</a>';
+            output +=       '<a onclick="movieSelected('+movie.id+')" class="btn btn-primary" href="#">Movie Details</a>';
             output +=   '</div>';
             output += '</div>';
         });
-
 
         if ( movies.length > 0 ) {
             $('#movies').html(output);
@@ -52,5 +51,54 @@ function getMovies(searchText) {
     function handleMissingImg(ele)
     {
         ele.src = '../../resources/images/poster-not-found.png';
+    }
+// -----------------------------------------------------------------------
+
+// -----------------------------------------------------------------------
+    function movieSelected(id)
+    {
+        sessionStorage.setItem('movieId', id);
+        window.location = 'movie.html';
+        return false;
+    }
+// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
+    function getMovie() {
+        var movieId = sessionStorage.getItem('movieId');
+            // Make a request to the ID
+            //https://api.themoviedb.org/3/movie/343611?api_key={api_key}
+            //https://api.themoviedb.org/3/movie/'+movieId+'?api_key=fa155f635119344d33fcb84fb807649b
+        axios.get('https://api.themoviedb.org/3/movie/'+movieId+'?api_key=fa155f635119344d33fcb84fb807649b')
+        .then(function (response) {
+            var movieDetail = response.data;
+            console.log(movieDetail);
+            var output = '\
+                <div class="row">\
+                    <div class="col-md-4">\
+                        <img onerror="handleMissingImg(this);" src="http://image.tmdb.org/t/p/w185/'+movieDetail.poster_path+'"> \
+                    </div>\
+                    <div class="col-md-8">\
+                        <h2>'+movieDetail.title+'</h2>\
+                        <p><strong>Overview: </strong>'+movieDetail.overview+'</p>\
+                        <ul class="list-group">\
+                            <li class="list-group-item"><strong>Release Date: </strong>'+movieDetail.release_date+'</li>\
+                            <li class="list-group-item"><strong>Popularity: </strong>'+movieDetail.popularity+'</li>\
+                            <li class="list-group-item"><strong>Vote Average: </strong>'+movieDetail.vote_average+'</li>\
+                        </ul>\
+                    </div>\
+                    <div class ="col-md-8">\
+                        <a href="index.html" class="btn btn-primary">Go Back To Search</a>\
+                        <a target="_blank" href="https://www.themoviedb.org/movie/'+movieDetail.id+'-'+movieDetail.title+'" class="btn btn-primary pull-right">Read More</a>\
+                    </div>\
+                </div>\
+                ';
+            $('#movieDetail').html(output);
+
+        })
+        .catch(function (error) {
+            console.log(error);
+            console.log('something is going wrong');
+        });
+        
     }
 // -----------------------------------------------------------------------
